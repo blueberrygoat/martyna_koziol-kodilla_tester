2090a769ae9e48d5be253f1a2681316c2e52046a
Martyna Koziol <martyna.olejnik1@gmail.com>
2023-10-01 18:03:30 +0200

    Zadanie: rozbudowa testów kalkulatora

---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Application.java
@@ -0,0 +1,37 @@
+package com.kodilla.basic_assertion;
+
+public class Application {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int a = 5;
+        int b = 8;
+        int sumResult = calculator.sum(a,b);
+        boolean correct = ResultChecker.assertEquals(13,sumResult);
+        if (correct){
+            System.out.println("Metoda sum działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sum nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int subResult = calculator.sub(a,b);
+        boolean correct1 = ResultChecker.assertEquals(-3,subResult);
+        if (correct1){
+            System.out.println("Metoda sub działa poprawnie dla liczb " + a + " i " + b);
+        } else {
+            System.out.println("Metoda sub nie działa poprawnie dla liczb " + a + " i " + b);
+        }
+        int squaredResult = calculator.squared(a);
+        boolean correct2 = ResultChecker.assertEquals(25,squaredResult);
+        if (correct2){
+            System.out.println("Metoda squared działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda squared nie działa poprawnie dla liczby " + a);
+        }
+        int squared2Result = (int) calculator.squared2(a);
+        boolean correct3 = ResultChecker.assertEquals(25,squared2Result);
+        if (correct3){
+            System.out.println("Metoda squared działa poprawnie dla liczby " + a);
+        } else {
+            System.out.println("Metoda squared nie działa poprawnie dla liczby " + a);
+        }
+    }
+}
---
+++ kodilla-basic-tests/src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -0,0 +1,17 @@
+package com.kodilla.basic_assertion;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+    public int squared(int a){
+        return a * a;
+    }
+    public double squared2(double a) {
+        return Math.pow(a, 2);
+    }
+}
